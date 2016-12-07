76c471b5423180b1d65d5c4691bb8e6dca602d9f
Thom Page <thom.page.000@gmail.com>
2016-12-07 13:25:47 -0500

    adds AJAX to omdbapi from input string

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,18 @@ console.log('hi');
 // ONLOAD
 $(function() {
 
- // AJAX REQUEST TO OMDBAPI
-  $.ajax('http://www.omdbapi.com/?t=eraserhead&y=&plot=short&r=json')
-   .done(function(result) {
-     console.log(result);
-   });
+  $('#input-submit').on('click', function() {
+
+    // INPUT VALUE STRING
+    var $inputString = $('#input-box').val();
+    // AJAX REQUEST TO OMDBAPI
+     $.ajax('http://www.omdbapi.com/?t=' + $inputString + '&y=&plot=short&r=json')
+      .done(function(result) {
+        console.log(result);
+      });
+
+  })
+
+
 
 });
