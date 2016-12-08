a4fd0b2224f2020283c70e0f2d341b1d66b10885
Thom Page <thom.page.000@gmail.com>
2016-12-08 16:51:22 -0500

    adds error message for movie not found

--- public/js/app.js
+++ public/js/app.js
@@ -15,40 +15,47 @@ $(function() {
         $('#result-container').empty();
         $('#input-box').val('');
 
-        var $movieContainer = $('<div>');
+        if (result.Error) {
 
-        $movieContainer.append($('<h3>').text(result.Title));
-        $movieContainer.append($('<p>').text(result.Year));
-        $movieContainer.append($('<p>').text(result.Director));
-        $movieContainer.append($('<img>').attr('src', result.Poster).addClass('poster-img'));
-        $movieContainer.append($('<p>'));
-        var $addButton = $('<button type="submit">ADD</button>');
-        $addButton.addClass('hit-button');
-        // $addButton.addClass('btn btn-lg-success');
-        $movieContainer.append($addButton);
+          $('#result-container').text(result.Error + ' Did you spell it correctly though? Maybe OMDB Api hasn\'t added it yet, which is a shame.' );
 
-        $movieContainer.addClass('col-lg-6 text-center');
+        } else {
 
-        $('#result-container').append($movieContainer);
+          var $movieContainer = $('<div>');
 
-        $addButton.on('click', function() {
-          // SEND MOVIE TO SERVER - CREATE MOVIE
+          $movieContainer.append($('<h3>').text(result.Title));
+          $movieContainer.append($('<p>').text(result.Year));
+          $movieContainer.append($('<p>').text(result.Director));
+          $movieContainer.append($('<img>').attr('src', result.Poster).addClass('poster-img'));
+          $movieContainer.append($('<p>'));
+          var $addButton = $('<button type="submit">ADD</button>');
+          $addButton.addClass('hit-button');
+          // $addButton.addClass('btn btn-lg-success');
+          $movieContainer.append($addButton);
 
-          var userId = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
+          $movieContainer.addClass('col-lg-6 text-center');
 
-          console.log('user is: ', userId);
+          $('#result-container').append($movieContainer);
 
-          $.ajax({
-            method: 'POST',
-            url: '/movies/' + userId,
-            data: result
-          }).done(function(savedUser) {
-            window.location.href = '/movies/' + savedUser._id + '/alphabetical_columns'
-            // console.log(savedUser);
-            // $addButton.remove();
-            // $('#input-box').val('');
-          });
-        }); // end add button handler
+          $addButton.on('click', function() {
+            // SEND MOVIE TO SERVER - CREATE MOVIE
+
+            var userId = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
+
+            console.log('user is: ', userId);
+
+            $.ajax({
+              method: 'POST',
+              url: '/movies/' + userId,
+              data: result
+            }).done(function(savedUser) {
+              window.location.href = '/movies/' + savedUser._id + '/alphabetical_columns'
+              // console.log(savedUser);
+              // $addButton.remove();
+              // $('#input-box').val('');
+            });
+          }); // end add button handler
+        } // end else error
       }); // end movie ajax
   }); // end input submit
 }); // end onload
