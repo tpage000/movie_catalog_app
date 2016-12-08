57dc7c5a6485f41341c50df3148d22c305370551
Thom Page <thom.page.000@gmail.com>
2016-12-08 18:57:25 -0500

    adds comments to users controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,58 +5,59 @@ var router = express.Router();
 // MODELS
 var User = require('../models/users');
 
-// INDEX to json
+// INDEX OF USERS - json output for testing
+// GET /users
 router.get('/', function(req, res) {
   User.find(function(err, allUsers) {
     res.send(allUsers);
   });
 });
 
-// signup
+// SIGNUP FORM
+// GET /users/signup
 router.get('/signup', function(req, res) {
   res.render('users/signup.ejs', { error: false });
 });
 
-// SHOW to view
+// MAIN PAGE
+// USER SHOW - loads a page to make AJAX requests to OMDB API
+// GET /users/:id
 router.get('/:id', function(req, res) {
   User.findById(req.params.id, function(err, userData) {
     res.render('users/show.ejs', { user: userData });
   });
 });
 
-// SHOW to json
+// USER SHOW - json output for testing
+// GET /users/:id/json
 router.get('/:id/json', function(req, res) {
   User.findById(req.params.id, function(err, userData) {
     res.send(userData);
   });
 });
 
+// CREATE USER - data comes from signup page
+// POST /users
 router.post('/', function(req, res) {
-
-  console.log('USER CREATE ROUTE HIT');
-
-  console.log('req.body is: ', req.body);
-
   User.create(req.body, function(err, newUser) {
+    // If user already exists, send error message to the page
     if (err) {
       res.render('users/signup.ejs', { error: true })
+    // Else go to the user's show page -- New Movie
     } else {
-      console.log('new user created: ', newUser);
       res.redirect('/users/' + newUser.id);
     }
   });
-});
+}); // end user create
 
+// USER LOGIN - data comes from signup page
+// POST /users/login
 router.post('/login', function(req, res) {
-
-  console.log('login route hit!');
-  console.log('req.body is: ', req.body);
-
   User.findOne({ name: req.body.name}, function(err, foundUser) {
       if (err) { console.log(err); }
       res.redirect('/users/' + foundUser.id);
   });
-
 });
 
+// EXPORT THE ROUTER - required in server.js and used as middleware for '/users'
 module.exports = router;
