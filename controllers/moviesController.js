667b7beb1ab99e9b88b2077687264c96837f2ea3
thom page <thom.page.000@gmail.com>
2016-12-07 21:45:46 -0500

    adds model virtuals and removes model processing from controllers

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -10,13 +10,7 @@ var User = require('../models/users');
 // GET /movies/:user_id/alphabetical
 router.get('/:user_id/alphabetical', function(req, res) {
   User.findById(req.params.user_id, function(err, foundUser) {
-    // non-destructive sorting of array objects by title
-    var sortedByTitle = foundUser.movies.slice().sort(function(a, b) {
-      if (a.Title < b.Title) { return -1; }
-      if (a.Title > b.Title) { return 1; }
-      return 0;
-    });
-    res.render('movies/index.ejs', { movies: sortedByTitle, userId: foundUser.id });
+    res.render('movies/index.ejs', { movies: foundUser.moviesAlphabetical, userId: foundUser.id });
   });
 });
 
@@ -24,13 +18,7 @@ router.get('/:user_id/alphabetical', function(req, res) {
 // GET /movies/:user_id/release_date
 router.get('/:user_id/release_date', function(req, res) {
   User.findById(req.params.user_id, function(err, foundUser) {
-    // non-destructive sorting of array objects by year
-    var sortedByYear = foundUser.movies.slice().sort(function(a, b) {
-      if (a.Year < b.Year) { return -1; }
-      if (a.Year > b.Year) { return 1; }
-      return 0;
-    });
-    res.render('movies/index_year.ejs', { movies: sortedByYear, userId: foundUser.id });
+    res.render('movies/index_year.ejs', { movies: foundUser.moviesChronological, userId: foundUser.id });
   });
 });
 
@@ -51,11 +39,8 @@ router.get('/:user_id/:movie_id', function(req, res) {
 // CREATE NEW MOVIE
 // POST /movies/:user_id
 router.post('/:user_id', function(req, res) {
-  console.log('CREATE MOVIE, data received: ', req.body);
-  console.log('^^ END OF DATA ^^');
   Movie.create(req.body, function(err, createdMovie) {
     User.findById(req.params.user_id, function(err, foundUser) {
-      console.log('The USER is: ', foundUser);
       foundUser.movies.push(createdMovie);
       foundUser.save(function(err, savedUser) {
         res.send(savedUser);
--- models/users.js
+++ models/users.js
@@ -10,6 +10,27 @@ var userSchema = new Schema({
   movies: [movieSchema]
 });
 
+userSchema.virtual('moviesAlphabetical').get(function() {
+  var sortedByTitle = this.movies.slice().sort(function(a, b) {
+    if (a.Title < b.Title) { return -1; }
+    if (a.Title > b.Title) { return 1; }
+    return 0;
+  });
+
+  return sortedByTitle;
+});
+
+userSchema.virtual('moviesChronological').get(function() {
+  var sortedByYear = this.movies.slice().sort(function(a, b) {
+    if (a.Year < b.Year) { return -1; }
+    if (a.Year > b.Year) { return 1; }
+    return 0;
+  });
+
+  return sortedByYear;
+});
+
+
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
