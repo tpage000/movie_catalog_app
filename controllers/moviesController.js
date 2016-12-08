4bf44b302de516a6f8bbd8f8f2f3ee2f38a11c69
Thom Page <thom.page.000@gmail.com>
2016-12-08 18:49:19 -0500

    reorders and comments movies controller

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -6,49 +6,56 @@ var router = express.Router();
 var Movie = require('../models/movies');
 var User = require('../models/users');
 
-// MOVIES INDEX ALPHABETICAL
-// GET /movies/:user_id/alphabetical
-router.get('/:user_id/alphabetical', function(req, res) {
-  User.findById(req.params.user_id, function(err, foundUser) {
-    res.render('movies/index.ejs', { movies: foundUser.moviesAlphabetical, userId: foundUser.id });
-  });
-});
-
-// MOVIES INDEX ALPHABETICAL
-// GET /movies/:user_id/alphabetical
+// MOVIES INDEX ALPHABETICAL AND WITH COLUMN HEADERS
+// GET /movies/:user_id/alphabetical_columns
 router.get('/:user_id/alphabetical_columns', function(req, res) {
   User.findById(req.params.user_id, function(err, foundUser) {
+    // alphabetical column movie data is provided using mongoose 'virtual' in models/users.js
     res.render('movies/index_columns_alpha.ejs', { movies: foundUser.moviesColumnsAlpha, userId: foundUser.id });
   });
 });
 
+// MOVIES INDEX CHRONOLOGICAL AND WITH COLUMN HEADERS
+// GET /movies/:user_id/chronological_columns
 router.get('/:user_id/chronological_columns', function(req, res) {
   User.findById(req.params.user_id, function(err, foundUser) {
+    // chronological column movie data is provided using mongoose 'virtual' in models/users.js
     res.render('movies/index_columns_year.ejs', { movies: foundUser.moviesColumnsYear, userId: foundUser.id });
   });
 });
 
-// MOVIES INDEX BY RELEASE DATE
+// MOVIES INDEX ALPHABETICAL
+// GET /movies/:user_id/alphabetical
+router.get('/:user_id/alphabetical', function(req, res) {
+  User.findById(req.params.user_id, function(err, foundUser) {
+    // alphabetical list of movie data is provided using mongoose 'virtual' in models/users.js
+    res.render('movies/index.ejs', { movies: foundUser.moviesAlphabetical, userId: foundUser.id });
+  });
+});
+
+// MOVIES INDEX CHRONOLOGICAL
 // GET /movies/:user_id/release_date
 router.get('/:user_id/release_date', function(req, res) {
   User.findById(req.params.user_id, function(err, foundUser) {
+    // chronological list of movie data is provided using mongoose 'virtual' in models/users.js
     res.render('movies/index_year.ejs', { movies: foundUser.moviesChronological, userId: foundUser.id });
   });
 });
 
-// MOVIES INDEX BY DATE WATCHED
+// MOVIES INDEX BY DATE WATCHED - not implemented
 // GET /movies/:user_id/date_watched
 router.get('/:user_id/date_watched', function(req, res) {
   res.send('this user\'s movies date watched');
 });
 
 
-// SHOW A MOVIE
+// SHOW A USER'S MOVIE
+// GET /movies/:user_id/:movie_id
 router.get('/:user_id/:movie_id', function(req, res) {
   Movie.findById(req.params.movie_id, function(err, foundMovie) {
     res.send(foundMovie);
-  })
-})
+  });
+});
 
 // CREATE NEW MOVIE
 // POST /movies/:user_id
@@ -61,7 +68,7 @@ router.post('/:user_id', function(req, res) {
       }); // end foundUser.save()
     }); // end User.findByID()
   }); // end Movie.create()
+}); // end create route
 
-}); // end create
-
+// EXPORT THE ROUTER - required in server.js and then used as middleware
 module.exports = router;
