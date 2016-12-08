5d341fd9891d9948b416c3ce135df0adeb76075b
Thom Page <thom.page.000@gmail.com>
2016-12-08 18:30:40 -0500

    adds comments to clarify ajax chain

--- public/js/app.js
+++ public/js/app.js
@@ -3,26 +3,28 @@ console.log('hi');
 // ONLOAD
 $(function() {
 
+  // =======================================================================================
+  // BEGIN PROCESS FOR WHEN INPUT SUBMIT IS CLICKED
   $('#input-submit').on('click', function() {
-
-    // INPUT VALUE STRING
+    // Get string value from the input box (the intended movie title)
     var $inputString = $('#input-box').val();
+    // ==========================================================================================
     // AJAX REQUEST TO OMDBAPI
      $.ajax('https://www.omdbapi.com/?t=' + $inputString + '&y=&plot=short&r=json')
       .done(function(result) {
         console.log(result);
-
+        // empty out the movie results and input box when another request comes in
         $('#result-container').empty();
         $('#input-box').val('');
-
+        // if the AJAX request to OMDB does not have the movie, send this message to the page
         if (result.Error) {
-
           $('#result-container').text(result.Error + ' Did you spell it correctly though? Maybe OMDB Api hasn\'t added it yet, which is a shame.' );
-
+        // if the AJAX request to OMDB returns a movie, add the movie details to the page
+        // and add a button so that user can put the movie in the user's collection
         } else {
-
+          // ====================================================================================
+          // MOVIE RESULTS DISPLAY
           var $movieContainer = $('<div>');
-
           $movieContainer.append($('<h3>').text(result.Title));
           $movieContainer.append($('<p>').text(result.Year));
           $movieContainer.append($('<p>').text(result.Director));
@@ -30,32 +32,35 @@ $(function() {
           $movieContainer.append($('<p>'));
           var $addButton = $('<button type="submit">ADD</button>');
           $addButton.addClass('hit-button');
-          // $addButton.addClass('btn btn-lg-success');
           $movieContainer.append($addButton);
-
           $movieContainer.addClass('col-lg-6 text-center');
-
           $('#result-container').append($movieContainer);
+          // END MOVIE RESULTS DISPLAY
+          // ===================================================================================
 
+          // ===================================================================================
+          // 'ADD' BUTTON FUNCTIONALITY
+          // When the 'add' button is clicked, send a POST request to the movie create route
           $addButton.on('click', function() {
-            // SEND MOVIE TO SERVER - CREATE MOVIE
-
+            // Takes the user's id from the url bar
             var userId = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-
-            console.log('user is: ', userId);
-
+            // AJAX request to the server with the user's id and the movie information
             $.ajax({
               method: 'POST',
               url: '/movies/' + userId,
               data: result
             }).done(function(savedUser) {
+              // When the request is done, redirect to the alphabetical list
               window.location.href = '/movies/' + savedUser._id + '/alphabetical_columns'
-              // console.log(savedUser);
-              // $addButton.remove();
-              // $('#input-box').val('');
             });
           }); // end add button handler
+          // END 'ADD' BUTTON FUNCTIONALITY
+          // ==================================================================================
         } // end else error
       }); // end movie ajax
+      // END AJAX REQUEST TO OMDB API
+      // =================================================================================
   }); // end input submit
+  // END PROCESS FOR WHEN INPUT SUBMIT IS CLICKED
+  // ===========================================================================
 }); // end onload
