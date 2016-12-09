18dcfcb8764a2d57fbee1cf50c438968c3c9a076
Thom Page <thom.page.000@gmail.com>
2016-12-08 21:19:12 -0500

    adds movie rating functionality

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -48,12 +48,19 @@ router.get('/:user_id/date_watched', function(req, res) {
   res.send('this user\'s movies date watched');
 });
 
+// SHOW MOVIE JSON
+router.get('/:movie_id/json', function(req, res) {
+  Movie.findById(req.params.movie_id, function(err, foundMovie) {
+    res.send(foundMovie);
+  });
+});
 
 // SHOW A USER'S MOVIE
 // GET /movies/:user_id/:movie_id
 router.get('/:user_id/:movie_id', function(req, res) {
   Movie.findById(req.params.movie_id, function(err, foundMovie) {
-    res.send(foundMovie);
+    // res.send(foundMovie);
+    res.render('movies/show.ejs', { movie: foundMovie, userId: req.params.user_id });
   });
 });
 
@@ -69,6 +76,23 @@ router.post('/:user_id', function(req, res) {
     }); // end User.findByID()
   }); // end Movie.create()
 }); // end create route
+
+
+// UPDATE MOVIE Rating - can probably make this a generalized update
+router.post('/:user_id/:movie_id/update_rating', function(req, res) {
+  console.log('hit the update rating route');
+  console.log(req.body);
+  Movie.findByIdAndUpdate(req.params.movie_id, { $set: req.body }, { new: true }, function(err, updatedMovie) {
+    console.log('updated movie: ', updatedMovie);
+    User.findById(req.params.user_id, function(err, foundUser) {
+      foundUser.movies.id(req.params.movie_id).remove();
+      foundUser.movies.push(updatedMovie);
+      foundUser.save(function(err, savedUser) {
+        res.redirect('/movies/' + req.params.user_id + '/' + updatedMovie.id);
+      });
+    });
+  });
+});
 
 // EXPORT THE ROUTER - required in server.js and then used as middleware
 module.exports = router;
