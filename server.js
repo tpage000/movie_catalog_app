aef36be2d58b4c987384ca6d768aff827367ebfa
Thom Page <thom.page.000@gmail.com>
2016-12-08 19:09:03 -0500

    refactors and comments server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express     = require('express'),
     mongoose    = require('mongoose');
 
 // PORT
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 // DB
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/movies_app'
@@ -13,13 +13,15 @@ mongoose.connect(mongoURI);
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.once('open', function() {
   console.log('DB: Connected');
-  // var data = { name: "Thom", password: "Thom" }
+  // ORIGINAL TESTING CODE FOR CREATING A USER DURING DEVELOPMENT
+  // var data = { name: "Brain Gremlin", password: "Brain Gremlin" }
   // var User = require('./models/users');
   // User.create(data, function(err, result) {
   //   if (err) { console.log(err) }
   //   console.log(result);
   //   mongoose.connection.close();
   // });
+  // END TESTING CODE
 });
 
 // CONTROLLERS
@@ -34,15 +36,14 @@ app.use(bodyParser.json());
 app.use('/users', usersController);
 app.use('/movies', moviesController);
 
-
 // ROOT
 app.get('/', function(req, res) {
   res.redirect('/users/signup');
 });
 
 // LISTENER
-app.listen(process.env.PORT || port, function() {
+app.listen(port, function() {
   console.log('=================================');
-  console.log('MOVIE APP RUNNING ON PORT: ', process.env.PORT || port);
+  console.log('MOVIE APP RUNNING ON PORT: ', port);
   console.log('=================================')
 });
