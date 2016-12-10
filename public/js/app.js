fd9ce7a7afd9cef7f9a1db783eb4f2b6379b1a99
thom page <thom.page.000@gmail.com>
2016-12-10 17:25:56 -0500

    updates omdb request to get search array rather than single title, and add third request to get single title afterwards with more detail

--- public/js/app.js
+++ public/js/app.js
@@ -16,59 +16,78 @@ $(function() {
     // ==========================================================================================
     // MAKE AJAX REQUEST TO OMDBAPI using the input string in the request url
      // Note: The request url needs to be https or Heroku gives a 'mixed content' error
-     $.ajax('https://www.omdbapi.com/?t=' + $inputString + '&y=&plot=short&r=json')
-      .done(function(result) {
-        // The returned data from OMDB is:
-        console.log(result);
-        // empty out the results box and input box when another request comes in
+     $.ajax('https://www.omdbapi.com/?s=' + $inputString + '&r=json')
+      .done(function(searchResults) {
+        console.log('OMDB SEARCH results: ', searchResults);
         $('#result-container').empty();
         $('#input-box').val('');
         // if the AJAX request to OMDB does not have the movie, send this message to the page
-        if (result.Error) {
-          $('#result-container').text(result.Error + ' Did you spell it correctly though? Maybe OMDB Api hasn\'t added it yet, which is a shame.' );
+        if (searchResults.Error) {
+          $('#result-container').text(searchResults.Error + ' Did you spell it correctly though? Maybe OMDB Api hasn\'t added it yet, which is a shame.' );
         // if the AJAX request to OMDB returns a movie, add the movie details to the page
         // and add a button so that user can put the movie in the user's collection
         } else {
           // ====================================================================================
           // MOVIE RESULTS DISPLAY
-          var $movieContainer = $('<div>');
-          $movieContainer.append($('<h3>').text(result.Title));
-          $movieContainer.append($('<p>').text(result.Year));
-          $movieContainer.append($('<p>').text(result.Director));
-          if (result.Poster == "N/A") { result.Poster = "https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/COMMUNITYTAGS/V5.0.a/root/projects/repository/config/alfresco/thumbnail/thumbnail_placeholder_256_qt.png"; };
-          $movieContainer.append($('<img>').attr('src', result.Poster).addClass('poster-img'));
-          $movieContainer.append($('<p>'));
-          var $addButton = $('<button type="submit">ADD</button>');
-          $addButton.addClass('hit-button');
-          $movieContainer.append($addButton);
-          $movieContainer.addClass('col-lg-6 text-center');
-          $('#result-container').append($movieContainer);
+          // All the results are in an array called .Search
+          searchResults.Search.forEach(function(result) {
+            var $movieContainer = $('<div>');
+            $movieContainer.append($('<h3>').text(result.Title));
+            $movieContainer.append($('<p>').text(result.Year));
+            if (result.Poster == "N/A") { result.Poster = "https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/COMMUNITYTAGS/V5.0.a/root/projects/repository/config/alfresco/thumbnail/thumbnail_placeholder_256_qt.png"; };
+            $movieContainer.append($('<img>').attr('src', result.Poster).addClass('poster-img'));
+            $movieContainer.append($('<p>'));
+            // Each movie's 'add' button will have the imdb ID as an id.
+            // This is used after the 'add' button is clicked, in order to make yet another ajax request,
+            // this time to the single-movie api in order to get more detailed results on the chosen movie.
+            var $addButton = $('<button id=' + result.imdbID + ' type="submit">ADD</button>');
+            $addButton.addClass('hit-button');
+            $addButton.on('click', getMoreMovieInfo);
+            $movieContainer.append($addButton);
+            $movieContainer.addClass('col-lg-4 text-center');
+            $('#result-container').append($movieContainer);
+          });
           // END MOVIE RESULTS DISPLAY
           // ===================================================================================
-
-          // ===================================================================================
-          // 'ADD' BUTTON FUNCTIONALITY
-          // When the 'add' button is clicked, send a POST request to the movie create route
-          $addButton.on('click', function() {
-            // Takes the user's id from the url bar
-            var userId = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-            // AJAX request to the server with the user's id and the movie information
-            $.ajax({
-              method: 'POST',
-              url: '/movies/' + userId,
-              data: result
-            }).done(function(response) {
-              // When the request is done, redirect to the new movie show page
-              window.location.href = '/movies/' + response.userId + '/' + response.movieId;
-            });
-          }); // end add button handler
-          // END 'ADD' BUTTON FUNCTIONALITY
-          // ==================================================================================
         } // end else error
-      }); // end movie ajax
-      // END AJAX REQUEST TO OMDB API
+      }); // end OMDB SEARCH
       // =================================================================================
   }); // end input submit
   // END PROCESS FOR WHEN INPUT SUBMIT IS CLICKED
   // ===========================================================================
 }); // end onload
+
+// ===================================================================================
+// 'ADD' BUTTON FUNCTIONALITY
+// When the 'add' button is clicked,
+//    1. Make another request to omdb with the imdb id, in order to get more detailed results for the particular movie
+//    2. Send a POST request to the movie create route
+var getMoreMovieInfo = function() {
+  // the imdb id is the 'add' button's id.
+  console.log(this.id);
+  // Retrieves detailed information about the chosen title using imdb id within the search string
+  $.ajax('https://www.omdbapi.com/?i=' + this.id + '&y=&plot=short&r=json')
+   .done(function(result) {
+     if (result.Error) {
+       $('#result-container').text(result.Error);
+       // if the AJAX request to OMDB returns a movie, add the movie details to the page
+     } else {
+       // ADDS THE CHOSEN TITLE TO THE USER'S COLLECTION
+       // Takes the user's id from the url bar
+       var userId = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
+       // AJAX request to the server with the user's id and the movie information
+       $.ajax({
+         method: 'POST',
+         url: '/movies/' + userId,
+         data: result
+       }).done(function(response) {
+         // When the request is done, redirect to the new movie show page
+         window.location.href = '/movies/' + response.userId + '/' + response.movieId;
+       });
+     } // end if error
+   }); // end movie ajax
+   // END AJAX REQUEST TO OMDB API
+} // end getMoreMovieInfo function
+
+// END 'ADD' BUTTON FUNCTIONALITY
+// ==================================================================================
